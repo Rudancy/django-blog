0a6720c069af9fb054dbd4afd5d31dc3ec4e52b0
Rudancy <45081024+Rudancy@users.noreply.github.com>
2020-04-22 14:25:51 +0100

    Delete env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", 'k&o1fa0br&mpv!!#8dgh9lm5oym_#&^m=6g__ark0or2=g60q(')
-
-
-os.environ.setdefault("DATABASE_URL", "postgres://htzfomkuqdnqos:188ed6870f40ca128fab170958c295b31b6fd43e854f90b0b3db936e5e68ce3f@ec2-54-247-89-181.eu-west-1.compute.amazonaws.com:5432/d85giiioeiue1j")
